c448699b08a21c0cdc32ee0ba41fcb22d41ad939
Alvin2800 <139653846+Alvin2800@users.noreply.github.com>
2026-03-28 22:59:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,35 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-temperature = 0
-humidity = 0
+temperature = 0.0
+humidity = 0.0
 emergency = 0
+
 logs = []
 
 @app.route("/")
 def home():
-    return "ESP32 IoT Server Running"
+    return "IoT API Alvin Running"
 
 @app.route("/data")
 def data():
-    global temperature, humidity, emergency
+    global temperature, humidity, emergency, logs
 
     temperature = float(request.args.get("temp", 0))
     humidity = float(request.args.get("hum", 0))
-    emergency= int(request.args.get("emergency",0))
+    emergency = int(request.args.get("emergency", 0))
+
     log_entry = {
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "temperature": temperature,
         "humidity": humidity,
         "emergency": emergency
     }
+
     logs.append(log_entry)
+
+    if len(logs) > 500:
+        logs = logs[-500:]
 
     print("Données reçues :", log_entry, flush=True)
 
@@ -38,7 +44,7 @@ def status():
         "humidity": humidity,
         "emergency": emergency
     })
-    
+
 @app.route("/logs")
 def get_logs():
     return jsonify(logs)
