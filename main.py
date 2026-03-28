324d80cf6ca345b44afd500275c944c42e3c4829
Alvin2800 <139653846+Alvin2800@users.noreply.github.com>
2026-03-28 22:18:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ def home():
 
 @app.route("/data")
 def data():
-    global temperature, humidity
+    global temperature, humidity, emergency
 
-    temperature = request.args.get("temp", 0)
-    humidity = request.args.get("hum", 0)
+    temperature = float(request.args.get("temp", 0))
+    humidity = float(request.args.get("hum", 0))
+    emergency= int(request.args.get("emergency",0))
+    log_entry = {
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "temperature": temperature,
+        "humidity": humidity,
+        "emergency": emergency
+    }
+    logs.append(log_entry)
 
-    print("Température :", temperature, flush=True)
-    print("Humidité :", humidity, flush=True)
+    print("Données reçues :", log_entry, flush=True)
 
     return "OK", 200
 
@@ -28,3 +35,11 @@ def get_temperature():
 @app.route("/humidity")
 def get_humidity():
     return jsonify({"humidity": humidity})
+    
+@app.route("/emergency")
+def get_emergency():
+    return jsonify({"Emergency": emergency})
+    
+@app.route("/logs")
+def get_logs():
+    return jsonify(logs)
